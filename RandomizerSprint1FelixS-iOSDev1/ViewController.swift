feaf32a7946501cbec6a0d95d19fc7ad312b55d0
felixMCC <nsotresmccpersonal@gmail.com>
2016-03-15 21:23:18 -0500

    Initial Commit

---
+++ RandomizerSprint1FelixS-iOSDev1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomizerSprint1FelixS-iOSDev1
+//
+//  Created by Nestor Sotres on 3/15/16.
+//  Copyright © 2016 Nestor Sotres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
